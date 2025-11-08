036c8507ba74eb8943f0f90f55360f9bc1aedf93
Han-Teng Liao <hanteng@gmail.com>
2025-11-08 17:52:55 +0800

    toggle.js

---
+++ toggle.js
@@ -0,0 +1,7 @@
+<script>
+window.quartoToggleColorScheme = function() {
+  const html = document.documentElement;
+  const current = html.getAttribute("data-bs-theme");
+  html.setAttribute("data-bs-theme", current === "dark" ? "light" : "dark");
+};
+</script>
